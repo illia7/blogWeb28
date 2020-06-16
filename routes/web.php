4e3d38d770a05232699262a1ab33e424858ac8f2
illia7 <65444121+illia7@users.noreply.github.com>
2020-06-16 17:27:30 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', IndexController::class)->name('home');
+Route::get('/', IndexController::class)->name('index');
 
 Route::get('/about', 'PageController@about')->name('about');
 
@@ -26,3 +26,34 @@ Route::get('/author/{key}', News_by_author_controller::class)->name('news_by_aut
 Route::get('/news/{id}', Single_news_controller::class)->name('single_news');
 
 Route::get('/category/{key}', News_by_category::class)->name('news_by_category');
+
+
+//ADMIN PANEL
+
+Route::get('/admin/add_news', 'Admin_news_Controller@add')->name('add_news_get');
+
+Route::post('/admin/add_news', 'Admin_news_Controller@save')->name('add_news_post');
+
+Route::get('/admin/edit_news/{id}', 'Admin_news_Controller@edit')->name('edit_news_get');
+
+Route::post('/admin/edit_news/{id}', 'Admin_news_Controller@edit_save')->name('edit_news_post');
+
+Route::get('/admin/delete_news', 'Admin_news_Controller@delete')->name('delete_news_get');
+
+Route::delete('/admin/delete_news', 'Admin_news_Controller@delete')->name('delete_news_delete');
+
+Route::get('/404', function(){
+	return view('404');
+})->name('404');
+
+Route::post('/mail_subscribed', MailSubscriberController::class)->name('mail_subscribed');
+
+Route::get('/send_mail', function(){
+	$mail = new \App\Mail\UserSubscribed();
+	$mail->subject = 'Welcome to news';
+	\Illuminate\Support\Facades\Mail::to('demch78@ukr.net')->send($mail);
+})->name('send_mail');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
